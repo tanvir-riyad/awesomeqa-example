c2673cfd896f36e47067532f7f9ab05029069e04
Tanvir Riyad <113178352+tanvirriyad@users.noreply.github.com>
2024-02-03 14:32:47 +0100

    add comments

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,10 +19,13 @@ ticket_repository = TicketRepository(filepath=TICKET_FILEPATH)
 
 
 @app.get("/tickets")
+
 async def get_tickets(
     limit: int = 20,
     ticket_repository: TicketRepository = Depends(lambda: ticket_repository),
 ):
+    """ get all the available tickets in the JSON file"""
+
     tickets = ticket_repository.get_tickets(limit)
     return JSONResponse(tickets, status_code=200)
 
@@ -30,12 +33,15 @@ async def get_tickets(
 async def close_ticket(id:str,
         ticket_repository: TicketRepository = Depends(lambda: ticket_repository)):
     
+    """ update the ticket status of the object the given ID """
+
     closed_ticket = ticket_repository.close_ticket(id)
     return JSONResponse(closed_ticket, status_code=201)
 
 @app.put("/remove_ticket/{id}")
 async def remove_ticket(id:str,
             ticket_repository: TicketRepository = Depends(lambda: ticket_repository)):
+        """ remove the objects of the given ID permanently from JSON file"""
     
         removed_ticket = ticket_repository.remove_ticket(id)
         return JSONResponse(removed_ticket, status_code=201)
--- backend/app/repositories/ticket_repository.py
+++ backend/app/repositories/ticket_repository.py
@@ -9,9 +9,11 @@ class TicketRepository:
             self.data = json.load(json_file)
 
     def get_tickets(self, limit: Optional[int] = None) -> list[dict]:
+        'fetch all the available tickets'
         return self.data["tickets"][:limit]
     
     def close_ticket(self, ticket_id:str):
+        ' change the status of the ticket object to closed'
         for obj in self.data['tickets']:
             if obj['id'] == ticket_id:
                 obj['status'] = 'closed'
@@ -19,10 +21,12 @@ class TicketRepository:
                 return obj
             
     def save_to_file(self):
+        'save the updates to the file'
         with open(self.filepath, 'w') as json_file:
             json.dump(self.data, json_file, indent=2)
 
     def remove_ticket(self, ticket_id:str):
+        'remove tickets from the json file'
         for obj in self.data['tickets']:
             if obj['id'] == ticket_id:
                 self.data['tickets'].remove(obj)
